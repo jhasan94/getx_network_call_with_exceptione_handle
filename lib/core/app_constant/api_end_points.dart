9d13a7fb56439542abe510a3d09d23be9d8cf621
jahidhasanm1 <jahidnsu142@gmail.com>
2024-11-19 09:35:26 +0300

    added constant file

---
+++ lib/core/app_constant/api_end_points.dart
@@ -0,0 +1,5 @@
+enum RequestType {get,post}
+
+class ApiEndPoints {
+  static String governorate = '/v2/front/home/governates';
+}
---
+++ lib/core/app_constant/app_status_code.dart
@@ -0,0 +1,9 @@
+class AppStatusCode {
+  static int created = 201;
+  static int success = 200;
+  static int updated = 202;
+  static int unAuthorized = 401;
+  static int badRequest = 400;
+  static int severError = 500;
+  static int notFound = 404;
+}
