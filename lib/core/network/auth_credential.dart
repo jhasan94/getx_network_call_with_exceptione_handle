9f6d305e8c5cf9687a70861f0b1f0520c6b4c540
jahidhasanm1 <jahidnsu142@gmail.com>
2024-11-19 09:37:17 +0300

    modify gitignore

---
+++ lib/core/network/auth_credential.dart
@@ -0,0 +1,5 @@
+class AuthCredential {
+  static const String username = 'mobileapikey';
+  static String password = 'j#7QGQui%sTpf2QC12*EoCcCc';
+  static String baseUrl = 'https://api.sakan.tech/api';
+}
