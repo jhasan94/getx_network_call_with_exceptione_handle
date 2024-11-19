4c4f8ee14c5df3a6470ec2d43d10106beed9a51d
jahidhasanm1 <jahidnsu142@gmail.com>
2024-11-19 09:40:26 +0300

    remove credential from repo

--- lib/core/network/auth_credential.dart
+++
@@ -1,5 +0,0 @@
-class AuthCredential {
-  static const String username = 'mobileapikey';
-  static String password = 'j#7QGQui%sTpf2QC12*EoCcCc';
-  static String baseUrl = 'https://api.sakan.tech/api';
-}
